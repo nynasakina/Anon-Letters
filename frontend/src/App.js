608fc140d4417c580df1e2f20e78d1d54c397263
Nyna Yusof <ngnaaa@gmail.com>
2022-11-16 11:38:05 +0800

    App js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-
-
+import Topbar from "./components/topbar/Topbar.js";
+import { Route, Routes } from "react-router";
+import Home from "./pages/home/Home.js";
+import Register from "./pages/register/Register.js";
+import Login from "./pages/login/Login.js";
+import Write from "./pages/write/Write.js";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import { useContext } from "react";
+import { Context } from "./context/Context.js";
 function App() {
+  const {user} = useContext(Context)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Topbar></Topbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single/>} />
+
+      </Routes>
+    </>
   );
 }
-
 export default App;
